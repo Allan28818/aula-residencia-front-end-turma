e9a87b7de2506d7d6867aa099b4b36c3d12e02bd
Vinicius Peixoto <vrp250606@gmail.com>
2025-06-03 20:20:59 -0300

    refactor(reverseButton): Changed the logic of the functions that involve the 'reverseButton' component

--- src/pages/reverse-button.tsx
+++ src/pages/reverse-button.tsx
@@ -3,9 +3,7 @@ import { ReverseButton } from "../components/inputs/password-input/reverse-butto
 export default function ReverseButtonVariant() {
   return (
     <div>
-      <ReverseButton variant="variant1">TEXT</ReverseButton>
-      <ReverseButton variant="variant2">TEXT</ReverseButton>
-      <ReverseButton variant="variant3">TEXT</ReverseButton>
+      <ReverseButton texto='TEXT'/>
     </div>
   );
-}
+} 
