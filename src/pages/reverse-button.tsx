84f585cc6e07ba95234851f067972d6ca5a65ff8
Vinicius Peixoto <vrp250606@gmail.com>
2025-06-02 21:58:10 -0300

    feat(reverseButton): Reverse buttons features were created

---
+++ src/pages/reverse-button.tsx
@@ -0,0 +1,11 @@
+import { ReverseButton } from "../components/inputs/password-input/reverse-button";
+
+export default function ReverseButtonVariant() {
+  return (
+    <div>
+      <ReverseButton variant="variant1">TEXT</ReverseButton>
+      <ReverseButton variant="variant2">TEXT</ReverseButton>
+      <ReverseButton variant="variant3">TEXT</ReverseButton>
+    </div>
+  );
+}
