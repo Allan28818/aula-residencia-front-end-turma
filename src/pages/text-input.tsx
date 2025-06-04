495592590035161c767896a7776f33356b7e02cc
OwJao <j.antonioassuncao@outlook.com>
2025-06-04 16:28:58 -0300

    commit

---
+++ src/pages/text-input.tsx
@@ -0,0 +1,11 @@
+import { TextInput } from "@/components/inputs/text-input/textinput";
+import { useState } from "react";
+
+export default function TextInputFunc() {
+
+  return (
+    <>
+      <TextInput/>
+    </>
+  );
+}
