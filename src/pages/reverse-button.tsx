b00dc140fa8013f7d030e36e865f10ab653c5a6a
Vinicius Peixoto <vrp250606@gmail.com>
2025-06-05 19:48:32 -0300

    fix(reverse-button): Fixed the issues related to the files names

--- src/pages/reverse-button.tsx
+++
@@ -1,9 +0,0 @@
-import { ReverseButton } from "../components/reverse-button/reverse-button";
-
-export default function ReverseButtonVariant() {
-  return (
-    <div>
-      <ReverseButton texto='TEXT'/>
-    </div>
-  );
-} 
